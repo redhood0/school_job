e1ae3d29cf9c690802681ac2718ceedf502ce0d3
redhood0 <robotman.top@gmail.com>
2020-06-26 17:52:14 +0800

    删除无用注释

--- src/main/java/com/hooli/work/handler/GlobalExceptionHandler.java
+++ src/main/java/com/hooli/work/handler/GlobalExceptionHandler.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -30,46 +31,6 @@ import java.util.Map;
 @Slf4j
 public class GlobalExceptionHandler {
     /**
-     * 自定义异常的处理，统一在这里捕获返回JSON格式的友好提示
-     *
-     * @param exception
-     * @return ResponseResult
-     */
-//    @ExceptionHandler(value = {JwtException.class})
-//    @ResponseBody
-//    public ResponseResult sendError(JwtException exception) {
-//        log.error(exception.getMessage());
-//        return ResponseResult.failure(exception.getResultCode());
-//    }
-
-    /**
-     * 自定义异常的处理，统一在这里捕获返回JSON格式的友好提示
-     *
-     * @param exception
-     * @return ResponseResult
-     */
-//    @ExceptionHandler(value = {CustomException.class})
-//    @ResponseBody
-//    public ResponseResult sendError(CustomException exception) {
-//        log.error(exception.getMessage());
-//        return ResponseResult.failure(exception.getResultCode());
-//    }
-
-    /**
-     * InvalidClaimException异常的处理
-     *
-     * @param exception
-     * @return ResponseResult
-     */
-//    @ExceptionHandler(value = {InvalidClaimException.class})
-//    @ResponseBody
-//    public ResponseResult sendError(InvalidClaimException exception) {
-//        log.error(exception.getMessage());
-//        return ResponseResult.failure(ResultCode.USER_TOKEN_EXPIRES);
-//    }
-
-
-    /**
      * NPE异常的处理
      *
      * @param exception
@@ -79,7 +40,7 @@ public class GlobalExceptionHandler {
     @ResponseBody
     public ResponseResult sendError(NullPointerException exception) {
         log.error(exception.getMessage());
-        return ResponseResult.failure(ResultCode.RESULT_CODE_DATA_NONE,"参数异常");
+        return ResponseResult.failure(ResultCode.RESULT_CODE_DATA_NONE, "参数异常");
     }
 
     /**
@@ -88,12 +49,12 @@ public class GlobalExceptionHandler {
      * @param exception
      * @return ResponseResult
      */
-//    @ExceptionHandler(value = {IOException.class})
-//    @ResponseBody
-//    public ResponseResult sendError(IOException exception) {
-//        log.error(exception.getMessage());
-//        return ResponseResult.failure(ResultCode.CAPTCHA_ERROR);
-//    }
+    @ExceptionHandler(value = {IOException.class})
+    @ResponseBody
+    public ResponseResult sendError(IOException exception) {
+        log.error(exception.getMessage());
+        return ResponseResult.failure(ResultCode.AUTHENTICATION_NO_LOGIN);
+    }
 
     /**
      * 参数认证处理
@@ -113,23 +74,21 @@ public class GlobalExceptionHandler {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errors);
     }
 
-
-    //    @ExceptionHandler(NullPointerException.class)
-//    ResponseEntity<String> handleConstraintViolationException(NullPointerException e) {
-//        System.out.println("===============================>>>>>>>>>>");
-//        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(e.getMessage());
-//    }
     @ExceptionHandler(value = {BuildingException.class})
     @ResponseBody
     public ResponseResult sendError(BuildingException exception) {
         return ResponseResult.failure(ResultCode.SYSTEM_INNER_ERROR, exception.getMessage());
     }
 
+    /**
+     * ServiceException处理
+     *
+     * @param
+     * @return
+     */
     @ExceptionHandler(value = {ServiceException.class})
     @ResponseBody
     public ResponseResult sendError(ServiceException exception) {
         return ResponseResult.failure(ResultCode.SYSTEM_INNER_ERROR, exception.getMessage());
     }
-
-
 }
